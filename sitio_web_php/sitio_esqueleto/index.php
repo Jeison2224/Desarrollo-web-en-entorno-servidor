2d90c4f5fba5c804f26d72a766492dd6f277d4d3
José Medrano <josemedrano1415@gmail.com>
2023-11-21 18:05:13 +0100

    inicio sitio web php

---
+++ sitio_web_php/sitio_esqueleto/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <div class="menu-hori">
+
+    </div>
+    <nav class="menu-verti">
+
+    </nav>
+    <main>
+
+    </main>
+    <aside>
+
+    </aside>
+    <footer>
+
+    </footer>
+</body>
+</html>
